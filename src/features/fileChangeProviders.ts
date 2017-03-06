3c18b2608a1856834738590a5125417914b33da1
tomSawkins <sawkins.tom@gmail.com>
2017-03-06 15:47:18 +1100

    Socket.io working

--- src/features/fileChangeProviders.ts
+++ src/features/fileChangeProviders.ts
@@ -7,71 +7,185 @@ import setText from '../utils/setText';
 import * as crypto from 'crypto';
 import * as path from 'path';
 
+let username = 'thomas'
+let updateTime = 500;
+let documents: { [id: string]: DocumentCache } = {};
+let changeCache: Events.ChangeEvent[] = [];
+let documentQueue = {};
+let updateTimer = null;
+let _server: CollaborationServer = null;
+
 function getChecksum(text: string) {
     return crypto.createHash('sha1').update(text).digest('hex');
 }
 
-
 function forwardDocumentChanges(server: CollaborationServer, _channel: OutputChannel): Disposable {
+    _server = server;
     let minChange: number = 2;
     let count: number = 0;
     let originalDocument: string;
-    let documents: { [id: string]: { text: string, checksum: string } } = {};
 
     //We should load all active documents into cache
 
-    var a = workspace.onDidOpenTextDocument((e) => {
-        if (!e.isUntitled) {
-            let text = e.getText();
-            documents[workspace.asRelativePath(e.fileName)] = { text: text, checksum: getChecksum(text) };
-        }
-    })
+    workspace.onDidOpenTextDocument(cacheFile);
 
-    var b = workspace.onDidCloseTextDocument((e) => {
+    workspace.onDidCloseTextDocument((e) => {
         if (!e.isUntitled) {
-            delete documents[workspace.asRelativePath(e.fileName)];
+            //delete documents[workspace.asRelativePath(e.fileName)];
         }
     });
 
+    // Cache all open documents
+    vscode.workspace.textDocuments.forEach(cacheFile)
+
+    updateTimer = setTimeout(runUpdate, updateTime);
 
     return workspace.onDidChangeTextDocument((event) => {
         if (event.document.languageId != "Log") {
             let { document } = event;
-            let text = document.getText();
             var documentPath = workspace.asRelativePath(document.fileName);
-
-
-            count += 1;
-            //_channel.appendLine(count.toString());
-            if (count >= minChange) {
-
-                count = 0;
-                //_channel.append(`${document.fileName}: `);
-                var a = event.contentChanges.forEach(x => {
-                    _channel.append(`Start: (${x.range.start.line},${x.range.start.character}) End: (${x.range.end.line},${x.range.end.character}) Length: ${x.rangeLength} Text: ${x.text}`);
-                });
-                //_channel.appendLine("\nbefore diff");
-
-                var out = diff(documents[documentPath].text, text);
-                var delta = diff.toDelta(out);
-
-                documents[documentPath] = { text: text, checksum: getChecksum(text) };
-                _channel.appendLine(delta);
-                server.sendEvent(new Events.ChangeEvent(
-                    documentPath, {
-                        delta: delta,
-                        checksum: documents[documentPath].checksum
-                    }, "elijah"));
-            }
+            documentQueue[documentPath] = document.getText();
         }
-
-
-
-        //check server up or return
-
-        //fire server event
     });
 }
+
+function runUpdate() {
+    for (var file in documentQueue) {
+        if (documentQueue.hasOwnProperty(file)) {
+            var text = documentQueue[file];
+            var doc = documents[file];
+            if (doc == null) continue;
+            let isSimpleMatch = documents[file].shadow.text == text;
+            if (isSimpleMatch) continue;
+            var out = diff(documents[file].shadow.text, text);
+            var delta = diff.toDelta(out);
+            var checksum = getChecksum(text);
+
+            // Create change event and cache
+            var event = new Events.ChangeEvent(
+                {
+                    relativeFileName: file,
+                    delta: delta,
+                    checksum: checksum,
+                    version: documents[file].shadow.version,
+                    lastRecievedVersion: doc.remoteVersion
+                }, username);
+            changeCache.push(event);
+
+            // Update the copy with the changes
+            documents[file].shadow.version += 1;
+            documents[file].shadow.text = text;
+        }
+    }
+    // empty the queue
+    documentQueue = {};
+
+    // Send what is in the cache
+    if (changeCache.length > 0 && _server != null) {
+        _server.sendEvents(changeCache);
+    }
+
+    updateTimer = setTimeout(runUpdate, updateTime);
+}
+
+export function applyChanges(event: Events.ChangeEvent) {
+    if (event.by == username) {
+        return;
+    }
+
+    var relativeFileName = event.data.relativeFileName;
+    var diffData = event.data;
+
+    let absoluteFilePath = path.join(vscode.workspace.rootPath, relativeFileName);
+    var doc = documents[relativeFileName];
+
+    // Get rid of events that we know have been recieved
+    changeCache = changeCache.filter((x, index) => {
+        x.data.version < diffData.lastRecievedVersion;
+    });
+
+    if (doc == null) {
+        vscode.workspace.openTextDocument(absoluteFilePath).then((z) => {
+            documents[relativeFileName] = doc = {
+                remoteVersion: 0,
+                shadow: { text: z.getText(), version: 0 },
+                shadowBackup: { text: z.getText(), version: 0 }
+            }
+        });
+    }
+
+    if (doc.remoteVersion > diffData.version) {
+        // We've already seen this change so just discard it.
+        return;
+    }
+
+    var docPatch = patch(doc.shadow.text, diffData.delta);
+    var patchedDoc = patch.apply(docPatch, doc.shadow.text)
+    var patchedChecksum = getChecksum(patchedDoc);
+
+    if (patchedChecksum != diffData.checksum) {
+        // failed to update properly
+        return;
+    }
+
+    // Update shadow with changes
+    documents[relativeFileName].remoteVersion += 1;
+    documents[relativeFileName].shadow.text = patchedDoc;
+
+    // Copy to shadow backup
+    documents[relativeFileName].shadowBackup.text = patchedDoc;
+    documents[relativeFileName].shadowBackup.version = documents[relativeFileName].shadow.version;
+
+    // attempt to patch actual file;
+    var isActiveFile = (vscode.window.activeTextEditor.document.uri.fsPath == absoluteFilePath);
+
+    if (isActiveFile) {
+        setDocumentChanges(vscode.window.activeTextEditor.document.getText(), diffData.delta);
+    } else {
+        vscode.workspace.openTextDocument(absoluteFilePath).then((z) => {
+            vscode.window.showTextDocument(z, null, false).then(x => {
+                setDocumentChanges(z.getText(), diffData.delta, x);
+            });
+
+        }, (b) => {
+            console.log(b);
+        });
+    }
+}
+
+function cacheFile(file: TextDocument) {
+    if (!file.isUntitled) {
+        let text = file.getText();
+        let copy = { text: text, version: 0, checksum: getChecksum(text) }
+        documents[workspace.asRelativePath(file.fileName)] = {
+            shadow: copy,
+            remoteVersion: 0,
+            shadowBackup: copy
+        }
+    }
+}
+
+function setDocumentChanges(text: string, delta: string, editor?: any) {
+    try {
+        var docPatch = patch(text, delta);
+        var patchedText = patch.apply(docPatch, text);
+        setText(patchedText, editor);
+    } catch (ex) {
+        console.log(ex);
+    }
+}
+
+export interface DocumentCache {
+    shadow: DocumentCopy
+    shadowBackup: DocumentCopy
+    remoteVersion: number
+}
+
+export interface DocumentCopy {
+    text: string
+    version: number
+}
+
 
 function forwardFileChanges(server: CollaborationServer, a: OutputChannel): Disposable {
 
@@ -96,27 +210,4 @@ export default function forwardChanges(server: CollaborationServer, c: OutputCha
     return Disposable.from(
         forwardDocumentChanges(server, c),
         forwardFileChanges(server, c));
-}
-
-
-export function applyChanges(releativeFileName: string, diff: Events.Diff) {
-    let absoluteFilePath = path.join(vscode.workspace.rootPath, releativeFileName);
-    var document = vscode.workspace.openTextDocument(absoluteFilePath).then((z) => {
-        let currentDocumentText = z.getText();
-        if(getChecksum(currentDocumentText) == diff.checksum){
-            console.log("file already matches checksum",diff.checksum);
-            return;
-        }
-
-        var documentPatch = patch(currentDocumentText, diff.delta);
-
-        vscode.window.showTextDocument(z, null, false).then(x => {
-            var patchedText = patch.apply(documentPatch, currentDocumentText);
-            setText(patchedText, x);
-        });
-
-    }, (b) => {
-        var a = "";
-    });
-
 }
